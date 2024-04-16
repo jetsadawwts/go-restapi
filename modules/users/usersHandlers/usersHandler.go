41f9d2049fa553dc06f5385eca902c933474cfff
Jetsadawwts <jetsadawwts@gmail.com>
2024-04-16 10:42:25 +0700

    Module sign in.

--- modules/users/usersHandlers/usersHandler.go
+++ modules/users/usersHandlers/usersHandler.go
@@ -11,11 +11,15 @@ import (
 type userHandlerErrCode string
 
 const (
-	SignUpCustomerErr userHandlerErrCode = "users-001"
+	SignUpCustomerErr  userHandlerErrCode = "users-001"
+	SignInErr          userHandlerErrCode = "users-002"
+	RefreshPassportErr userHandlerErrCode = "users-003"
 )
 
 type IUsersHandler interface {
 	SignUpCustomer(c *fiber.Ctx) error
+	SignIn(c *fiber.Ctx) error
+	RefreshPassport(c *fiber.Ctx) error
 }
 
 type usersHandler struct {
@@ -76,4 +80,46 @@ func (h *usersHandler) SignUpCustomer(c *fiber.Ctx) error {
 	}
 
 	return entities.NewResponse(c).Success(fiber.StatusCreated, result).Res()
+}
+
+func (h *usersHandler) SignIn(c *fiber.Ctx) error {
+	req := new(users.UserCredential)
+	if err := c.BodyParser(req); err != nil {
+		return entities.NewResponse(c).Error(
+			fiber.ErrBadRequest.Code,
+			string(SignInErr),
+			err.Error(),
+		).Res()
+	}
+
+	passport, err := h.usersUsecase.GetPassport(req)
+	if err != nil {
+		return entities.NewResponse(c).Error(
+			fiber.ErrBadRequest.Code,
+			string(SignInErr),
+			err.Error(),
+		).Res()
+	}
+	return entities.NewResponse(c).Success(fiber.StatusOK, passport).Res()
+}
+
+func (h *usersHandler) RefreshPassport(c *fiber.Ctx) error {
+	req := new(users.UserRefreshCredential)
+	if err := c.BodyParser(req); err != nil {
+		return entities.NewResponse(c).Error(
+			fiber.ErrBadRequest.Code,
+			string(RefreshPassportErr),
+			err.Error(),
+		).Res()
+	}
+
+	passport, err := h.usersUsecase.RefreshPassport(req)
+	if err != nil {
+		return entities.NewResponse(c).Error(
+			fiber.ErrBadRequest.Code,
+			string(RefreshPassportErr),
+			err.Error(),
+		).Res()
+	}
+	return entities.NewResponse(c).Success(fiber.StatusOK, passport).Res()
 }
---
+++ pkg/auth/auth.go
@@ -0,0 +1,137 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jetsadawwts/go-restapi/config"
+	"github.com/jetsadawwts/go-restapi/modules/users"
+)
+
+type TokenType string
+
+const (
+	Access  TokenType = "access"
+	Refresh TokenType = "refresh"
+	Admin   TokenType = "admin"
+	ApiKey  TokenType = "apikey"
+)
+
+type auth struct {
+	mapClaims *mapClaims
+	cfg       config.IJwtConfig
+}
+
+type mapClaims struct {
+	Claims *users.UserClaims `json:"claims"`
+	jwt.RegisteredClaims
+}
+
+type IAuth interface {
+	SignToken() string
+}
+
+func jwtTimeDurationCal(t int) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Now().Add(time.Duration(int64(t) * int64(math.Pow10(9)))))
+}
+
+func jwtTimeRepeatAdapter(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Unix(t, 0))
+}
+
+func (a *auth) SignToken() string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, a.mapClaims)
+	ss, _ := token.SignedString(a.cfg.SecretKey())
+	return ss
+}
+
+func ParseToken(cfg config.IJwtConfig, tokenString string) (*mapClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &mapClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("signing method is invalid")
+		}
+		return cfg.SecretKey(), nil
+	})
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenMalformed) {
+			return nil, fmt.Errorf("token format is invalid")
+		} else if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, fmt.Errorf("token had expired")
+		} else {
+			return nil, fmt.Errorf("parse token failed: %v", err)
+		}
+	}
+
+	if claims, ok := token.Claims.(*mapClaims); ok {
+		return claims, nil
+	} else {
+		return nil, fmt.Errorf("claims type is invalid")
+	}
+}
+
+func RepeatToken(cfg config.IJwtConfig, claims *users.UserClaims, exp int64) string {
+	obj := &auth{
+		cfg: cfg,
+		mapClaims: &mapClaims{
+			Claims: claims,
+			RegisteredClaims: jwt.RegisteredClaims{
+				Issuer:    "shop-api",
+				Subject:   "refresh-token",
+				Audience:  []string{"customer", "admin"},
+				ExpiresAt: jwtTimeRepeatAdapter(exp),
+				NotBefore: jwt.NewNumericDate(time.Now()),
+				IssuedAt:  jwt.NewNumericDate(time.Now()),
+			},
+		},
+	}
+
+	return obj.SignToken()
+}
+
+func NewAuth(TokenType TokenType, cfg config.IJwtConfig, claims *users.UserClaims) (IAuth, error) {
+	switch TokenType {
+	case Access:
+		return newAccessToken(cfg, claims), nil
+	case Refresh:
+		return newRefreshToken(cfg, claims), nil
+	default:
+		return nil, fmt.Errorf("unknow token type.")
+	}
+}
+
+func newAccessToken(cfg config.IJwtConfig, claims *users.UserClaims) IAuth {
+	return &auth{
+		cfg: cfg,
+		mapClaims: &mapClaims{
+			Claims: claims,
+			RegisteredClaims: jwt.RegisteredClaims{
+				Issuer:    "shop-api",
+				Subject:   "access-token",
+				Audience:  []string{"customer", "admin"},
+				ExpiresAt: jwtTimeDurationCal(cfg.AccessExpiresAt()),
+				NotBefore: jwt.NewNumericDate(time.Now()),
+				IssuedAt:  jwt.NewNumericDate(time.Now()),
+			},
+		},
+	}
+}
+
+func newRefreshToken(cfg config.IJwtConfig, claims *users.UserClaims) IAuth {
+	return &auth{
+		cfg: cfg,
+		mapClaims: &mapClaims{
+			Claims: claims,
+			RegisteredClaims: jwt.RegisteredClaims{
+				Issuer:    "shop-api",
+				Subject:   "refresh-token",
+				Audience:  []string{"customer", "admin"},
+				ExpiresAt: jwtTimeDurationCal(cfg.RefreshExpiresAt()),
+				NotBefore: jwt.NewNumericDate(time.Now()),
+				IssuedAt:  jwt.NewNumericDate(time.Now()),
+			},
+		},
+	}
+}
